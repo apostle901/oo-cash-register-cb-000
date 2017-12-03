e70ae7d143eea40992f7a424fc8df9b88c6d315c
Jawann <jawann@gmail.com>
2017-12-03 23:14:23 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,18 @@
+class CashRegister
+  attr_accessor :total
+  attr_reader :discount
 
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+  end
+
+  def add_item(name, amount, quantity = 1)
+    self.total += amount * quantity
+  end
+
+  def apply_discount
+    @total *= (1 - @discount/100.0)
+    @total.to_i
+  end
+end
